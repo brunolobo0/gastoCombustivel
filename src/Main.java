5de344d561853bc419b2715036217482ddd1adea
Bruno Lobo <brunosvlobo@gmail.com>
2025-06-23 17:21:30 -0300

    Upload de desafio

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+
+        int tempoViagem = sc.nextInt();
+        int velocidade = sc.nextInt();
+        double combustivel;
+
+        combustivel = (double) (tempoViagem * velocidade) / 12;
+
+        System.out.printf("%.3f%n", combustivel);
+
+        sc.close();
+
+    }
+
+}
